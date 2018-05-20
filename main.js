b1e6596f74db94c5a76b3693c25c35fca645bae9
mailwodan <mail.wodan@gmail.com>
2018-05-20 18:44:54 +0300

    List is ready
    List is ready

--- main.js
+++ main.js
@@ -1,25 +1,49 @@
-var ref = firebase.database().ref('www');
-ref.on('child_added', function(val1){
-    //console.log(val1.val());
-    console.log(val1);    
-});
+var ref = firebase.database().ref('sitters');
+// ref.on('child_added', function(val1){
+//     //console.log(val1.val());
+//     console.log(val1);    
+// });
 
-ref.on('value', function(val1){
-    //console.log(val1.val());
-    console.log(val1);    
-});
+// ref.on('value', function(val1){
+//     //console.log(val1.val());
+//     console.log(val1);    
+// });
 
-new Vue({
+var testApp = new Vue({
     el: '#test',
     data: {
-        test: 'KKKKKKK'
+        sitter: {
+            name: '',
+            age: ''
+        },
+        sitterList: {
+        }
     },
     methods: {
-        addData(){
+        addSitter(name, age) {
             ref.push({
-                fff: 'rrtrtr',
-                gggg: 'saaaaaaaaaaaaa'
+                name: name,
+                age: age
             });
+            this.sitter.name = '';
+            this.sitter.age = '';
+        },
+        deleteItem(itemKey){
+            console.log(itemKey);
+            ref.child(itemKey).remove();
+        },
+        saveItem(itemValue, itemKey){
+            ref.child(itemKey).set({
+                name: itemValue.name,
+                age: itemValue.age
+            }); 
         }
-    }
-})
+    },
+});
+
+ref.on('value', function (val1) {
+    console.log(val1.val());
+    testApp.sitterList = val1.val();
+});
+
+
